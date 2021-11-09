7d227eb427b8ee00dbcbfb85613bd8dc93b8ff21
vanhelsing7787 <94009636+vanhelsing7787@users.noreply.github.com>
2021-11-10 04:57:15 +0700

    Delete verus.sh

--- verus.sh
+++
@@ -1,5 +0,0 @@
-# Verus Coin
-sudo apt update
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
-tar -xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RFKUDcS8B6nXZQvA6JRFnwMM7QDLA6TfHC.Z1_Foxtrot -p x --cpu 36
